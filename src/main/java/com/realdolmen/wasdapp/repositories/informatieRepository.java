f1d14176c4ad55cfcd91f64b818e5f2a26813fc4
demunter jelter <demunterjelter@hotmail.com>
2018-11-26 15:42:34 +0100

    addressservice test van findAll en findByID

--- src/main/java/com/realdolmen/wasdapp/repositories/informatieRepository.java
+++ src/main/java/com/realdolmen/wasdapp/repositories/informatieRepository.java
@@ -11,4 +11,18 @@ package com.realdolmen.wasdapp.repositories;
  */
 public class informatieRepository {
     
+    public static final String TITEL = "titel";
+    public static final String LOCATIE = "locatie";
+    public static final String STRAAT = "straat";
+    public static final String NUMMER = "nummer";
+    public static final String POSTCODE = "postcode";
+    public static final String GEMEENTE = "gemeente";
+    public static final String LAND = "land";
+    public static final String OMSCHRIJVING = "omschrijving";
+    public static final String WIKIPEDIA_LINK = "wikipedia_link";
+    public static final String WEBSITE = "website";
+    public static final String TELEFOON = "telefoon";
+    public static final String E_MAIL = "e_mail";
+    public static final String PRIJS = "prijs";
+    public static final String PERSOON = "persoon";
 }
