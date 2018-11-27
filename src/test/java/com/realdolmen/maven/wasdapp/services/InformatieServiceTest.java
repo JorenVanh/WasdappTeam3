6977a734ccec4a8cd543ca229f131c9cc10e7617
Laurena1997 <laurena.nijs@realdolmen.com>
2018-11-27 15:35:14 +0100

    testFindAll() gemaakt en werkt

--- src/test/java/com/realdolmen/maven/wasdapp/services/InformatieServiceTest.java
+++ src/test/java/com/realdolmen/maven/wasdapp/services/InformatieServiceTest.java
@@ -5,10 +5,48 @@
  */
 package com.realdolmen.maven.wasdapp.services;
 
+import com.realdolmen.wasdapp.domain.Informatie;
+import com.realdolmen.wasdapp.repositories.InformatieRepository;
+import com.realdolmen.wasdapp.services.InformatieService;
+import java.util.ArrayList;
+import java.util.List;
+import static org.junit.Assert.assertEquals;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import org.mockito.runners.MockitoJUnitRunner;
+
 /**
  *
- * @author KDLBL62
+ * @author LNSBG31
  */
+@RunWith(MockitoJUnitRunner.class)
 public class InformatieServiceTest {
+     //Object to test
+    private InformatieService informatieService;
+    //Object(s) not to test but needed for the object to test => Mocks
+    @Mock
+    private InformatieRepository informatieRepository;
+    
+    @Before
+    public void init() {
+        informatieService = new InformatieService(informatieRepository);
+    }
+    
+     @Test
+    public void testFindAll() throws Exception {
+        //init data to test
+        List<Informatie> infos = new ArrayList<>();
+        when(informatieRepository.findAll()).thenReturn(infos);
+        //test the method
+        List<Informatie> result = informatieService.findAll();
+        //verify the results
+        assertEquals(result, infos);
+        verify(informatieRepository,times(1)).findAll();
+    }
     
 }
