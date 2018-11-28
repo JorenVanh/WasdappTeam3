77d06767f3fe7b83b2565fd3ec6da519890056ca
Laurena1997 <laurena.nijs@realdolmen.com>
2018-11-28 10:45:13 +0100

    QR Generator code uit commentaar gehaald, build success

--- src/main/java/com/realdolmen/wasdapp/QRGenerator.java
+++ src/main/java/com/realdolmen/wasdapp/QRGenerator.java
@@ -7,6 +7,7 @@ import com.google.zxing.WriterException;
 import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
 import com.google.zxing.qrcode.QRCodeWriter;
+import java.io.ByteArrayOutputStream;
 
 import java.io.IOException;
 import java.nio.file.FileSystems;
@@ -28,15 +29,15 @@ public class QRGenerator {
         MatrixToImageWriter.writeToPath(bitMatrix, "PNG", path);
     }
     
-//    private byte[] getQRCodeImage(String text, int width, int height) throws WriterException, IOException {
-//    QRCodeWriter qrCodeWriter = new QRCodeWriter();
-//    BitMatrix bitMatrix = qrCodeWriter.encode(text, BarcodeFormat.QR_CODE, width, height);
-//    
-//    ByteArrayOutputStream pngOutputStream = new ByteArrayOutputStream();
-//    MatrixToImageWriter.writeToStream(bitMatrix, "PNG", pngOutputStream);
-//    byte[] pngData = pngOutputStream.toByteArray(); 
-//    return pngData;
-//}
+    private byte[] getQRCodeImage(String text, int width, int height) throws WriterException, IOException {
+    QRCodeWriter qrCodeWriter = new QRCodeWriter();
+    BitMatrix bitMatrix = qrCodeWriter.encode(text, BarcodeFormat.QR_CODE, width, height);
+    
+    ByteArrayOutputStream pngOutputStream = new ByteArrayOutputStream();
+    MatrixToImageWriter.writeToStream(bitMatrix, "PNG", pngOutputStream);
+    byte[] pngData = pngOutputStream.toByteArray(); 
+    return pngData;
+}
 
     public static void main(String[] args) throws IOException {
         try {
