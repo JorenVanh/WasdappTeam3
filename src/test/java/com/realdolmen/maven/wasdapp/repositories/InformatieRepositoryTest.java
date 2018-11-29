9b276a3c47c601f1531c49ce82f33ea67e3236b5
Laurena1997 <laurena.nijs@realdolmen.com>
2018-11-29 11:07:26 +0100

    test geschreven createObjectTestFail() in InformatieRepositoryTest
    test geslaagd

--- src/test/java/com/realdolmen/maven/wasdapp/repositories/InformatieRepositoryTest.java
+++ src/test/java/com/realdolmen/maven/wasdapp/repositories/InformatieRepositoryTest.java
@@ -17,6 +17,7 @@ import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static org.mockito.Mockito.when;
 import org.mockito.runners.MockitoJUnitRunner;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertThat;
 
@@ -90,7 +91,58 @@ public class InformatieRepositoryTest {
         verify(resultSet, times(1)).getString(InformatieRepository.E_MAIL);
         verify(resultSet, times(1)).getDouble(InformatieRepository.PRIJS);
     }
-   
+    
+    @Test
+    public void createObjectTestFail() throws SQLException, NoQueryPossibleException{
+         //init data
+        when(resultSet.getString(InformatieRepository.KEY)).thenReturn("titel");
+        when(resultSet.getString(InformatieRepository.LOCATIE)).thenReturn("locatie");
+        when(resultSet.getString(InformatieRepository.STRAAT)).thenReturn("straat");
+        when(resultSet.getString(InformatieRepository.NUMMER)).thenReturn("nummer");
+        when(resultSet.getString(InformatieRepository.POSTCODE)).thenReturn("postcode");
+        when(resultSet.getString(InformatieRepository.GEMEENTE)).thenReturn("gemeente");
+        when(resultSet.getString(InformatieRepository.LAND)).thenReturn("land");
+        when(resultSet.getString(InformatieRepository.OMSCHRIJVING)).thenReturn("omschrijving");
+        when(resultSet.getString(InformatieRepository.WIKIPEDIA_LINK)).thenReturn("wikipedia_link");
+        when(resultSet.getString(InformatieRepository.WEBSITE)).thenReturn("website");
+        when(resultSet.getString(InformatieRepository.TELEFOON)).thenReturn("telefoon");
+        when(resultSet.getString(InformatieRepository.E_MAIL)).thenReturn("email");
+        when(resultSet.getString(InformatieRepository.PERSOON)).thenReturn("persoon");
+        when(resultSet.getDouble(InformatieRepository.PRIJS)).thenReturn(1.0);
+        
+        //test the test objects
+        Informatie result = informatieRepository.createObject(resultSet);
+        //verify the result
+        assertNotEquals("title", result.getTitel());
+        assertNotEquals("locaties", result.getLocatie());
+        assertNotEquals("straten", result.getStraat());
+        assertNotEquals("nummers", result.getNummer());
+        assertNotEquals("postcodes", result.getPostcode());
+        assertNotEquals("gemeenten", result.getGemeente());
+        assertNotEquals("landen", result.getLand());
+        assertNotEquals("omschrijvingen", result.getOmschrijving());
+        assertNotEquals("wikipedia_linken", result.getWiki_link());
+        assertNotEquals("websites", result.getWebsite());
+        assertNotEquals("telefoons", result.getTelefoon());
+        assertNotEquals("emails", result.getEmail());
+        assertThat(2.0, CoreMatchers.not(CoreMatchers.equalTo(result.getPrijs())));
+       
+        verify(resultSet, times(1)).getString(InformatieRepository.KEY);
+        verify(resultSet, times(1)).getString(InformatieRepository.LOCATIE);
+        verify(resultSet, times(1)).getString(InformatieRepository.STRAAT);
+        verify(resultSet, times(1)).getString(InformatieRepository.NUMMER);
+        verify(resultSet, times(1)).getString(InformatieRepository.POSTCODE);
+        verify(resultSet, times(1)).getString(InformatieRepository.GEMEENTE);
+        verify(resultSet, times(1)).getString(InformatieRepository.LAND);
+        verify(resultSet, times(1)).getString(InformatieRepository.KEY);
+        verify(resultSet, times(1)).getString(InformatieRepository.OMSCHRIJVING);
+        verify(resultSet, times(1)).getString(InformatieRepository.WIKIPEDIA_LINK);
+        verify(resultSet, times(1)).getString(InformatieRepository.WEBSITE);
+        verify(resultSet, times(1)).getString(InformatieRepository.TELEFOON);
+        verify(resultSet, times(1)).getString(InformatieRepository.E_MAIL);
+        verify(resultSet, times(1)).getDouble(InformatieRepository.PRIJS);
+    }
+    
      @Test
     public void createObjectTestThrowsSQLException() throws SQLException {
         //init data
