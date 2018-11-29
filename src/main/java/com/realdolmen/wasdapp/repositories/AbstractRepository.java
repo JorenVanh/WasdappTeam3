68c7f44951ea7312bf7a79a61f45f598892dd4e7
JorenVanh <joren.vanherwegen@hotmail.com>
2018-11-29 15:28:54 +0100

    updated repo to work with opencsv

--- src/main/java/com/realdolmen/wasdapp/repositories/AbstractRepository.java
+++ src/main/java/com/realdolmen/wasdapp/repositories/AbstractRepository.java
@@ -18,7 +18,7 @@ import java.util.List;
  * @author demun
  */
 public abstract class AbstractRepository<C,T> {
-     public static final String LOGIN = "root";
+    public static final String LOGIN = "root";
     public static final String PASSWORD = "root";
     public static String DRIVER = "com.mysql.jdbc.Driver";
     public static String URL = "jdbc:mysql://localhost:3306/wasdapp?autoReconnect=true&useSSL=false&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
@@ -38,7 +38,7 @@ public abstract class AbstractRepository<C,T> {
         this.url = url;
     }
       public Connection createConnection() throws SQLException {
-          System.out.println("trying connection");
+          System.out.println("Setting up connection...");
         return DriverManager.getConnection(url, LOGIN, PASSWORD);
     }
       
@@ -57,54 +57,9 @@ public abstract class AbstractRepository<C,T> {
         }
         return listToFill;
     }
-/*
-    public C findById(T id) throws NoQueryPossibleException {
-        C object = null;
-        try (Connection connection = createConnection()) {
-            PreparedStatement pstatement = connection.prepareStatement("SELECT * FROM " + tableName + " WHERE " + idName + " = " + id);
-            ResultSet resultSet = pstatement.executeQuery();
-            if (resultSet.next()) {
-                System.out.println("in resultset");
-                object = createObject(resultSet);
-            }
-        } catch (Exception e) {
-            //TODO Test
-            throw new NoQueryPossibleException("Find by id " + tableName + " can not be excecuted");
-        }
-        return object;
-    }*/
-public void insertItems(ArrayList<C> infos) throws NoQueryPossibleException{
-        //Only tested for postalcode
-        try (Connection connection = createConnection()) {
-            for(int i = 0; i <= infos.size()-1 ; i++ ){
-            String query = "REPLACE INTO " + tableName + getColumnString() + " values " + getValuesString(infos.get(i));
-            System.out.println(query);
-            PreparedStatement pstatement = connection.prepareStatement(query);
-            pstatement.executeUpdate();
-            
-        }
-            
-                        
-            /*String max = "SELECT max(" + idName + ") AS max FROM " + tableName;
-            System.out.println(max);
-            pstatement = connection.prepareStatement(max);
-            ResultSet resultSet = pstatement.executeQuery();
-            T r = null;
-            if (resultSet.next()) {
-                System.out.println("in resultset");
-                r = (T) resultSet.getObject(1);
-            }*/
-        } catch (Exception e) {
-            e.printStackTrace();
-            throw new NoQueryPossibleException("Insert " + tableName + " can not be excecuted ");
-        }
-    }
-    
      public abstract C createObject(ResultSet resultSet);
      
      public abstract String getColumnString();
-    
-    public abstract String getValuesString(C c);
 
     public String getUrl() {
         return url;
--- src/main/java/com/realdolmen/wasdapp/repositories/InformatieRepository.java
+++ src/main/java/com/realdolmen/wasdapp/repositories/InformatieRepository.java
@@ -6,8 +6,8 @@
 package com.realdolmen.wasdapp.repositories;
 
 import com.realdolmen.wasdapp.domain.Informatie;
+import com.realdolmen.wasdapp.GUI;
 import com.realdolmen.wasdapp.exceptions.NoQueryPossibleException;
-//import static com.realdolmen.wasdapp.repositories.InformatieRepository.TITEL;
 import static com.realdolmen.wasdapp.repositories.InformatieRepository.LOCATIE;
 import static com.realdolmen.wasdapp.repositories.InformatieRepository.STRAAT;
 import static com.realdolmen.wasdapp.repositories.InformatieRepository.NUMMER;
@@ -21,17 +21,16 @@ import static com.realdolmen.wasdapp.repositories.InformatieRepository.TELEFOON;
 import static com.realdolmen.wasdapp.repositories.InformatieRepository.E_MAIL;
 import static com.realdolmen.wasdapp.repositories.InformatieRepository.PRIJS;
 import static com.realdolmen.wasdapp.repositories.InformatieRepository.PERSOON;
-
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.util.ArrayList;
 
 public class InformatieRepository extends AbstractRepository<Informatie, String>{
     
     public static final String KEY = "titel";
     public static final String TABLE_NAME = "informatie";
-    //public static final String TITEL = "titel";
     public static final String LOCATIE = "locatie";
     public static final String STRAAT = "straat";
     public static final String NUMMER = "nummer";
@@ -67,26 +66,55 @@ public class InformatieRepository extends AbstractRepository<Informatie, String>
             informatie.setEmail(resultSet.getString(E_MAIL));
             informatie.setPersoon(resultSet.getString(PERSOON));
             informatie.setPrijs(resultSet.getDouble(PRIJS));
-
-             return informatie;
-            
+            return informatie;    
         } catch (SQLException e) {
             return null;
         }
         
     }
+    
+    public void insertItems(ArrayList<Informatie> infos) throws NoQueryPossibleException{
+        try (Connection connection = createConnection()) {
+            System.out.println("Connection succes");
+            for(int i = 0; i <= infos.size()-1 ; i++ ){
+            PreparedStatement pstatement = connection.prepareStatement("REPLACE INTO " + TABLE_NAME + getColumnString() + " values (?,?,?,?,?,?,?,?,?,?,?,?,?,?)");
+            pstatement.setString(1, infos.get(i).getTitel());
+            pstatement.setString(2, infos.get(i).getLocatie());
+            pstatement.setString(3, infos.get(i).getStraat());
+            pstatement.setString(4, infos.get(i).getNummer());
+            pstatement.setString(5, infos.get(i).getPostcode());
+            pstatement.setString(6, infos.get(i).getGemeente());
+            pstatement.setString(7, infos.get(i).getLand());
+            pstatement.setString(8, infos.get(i).getOmschrijving());
+            pstatement.setString(9, infos.get(i).getWiki_link());
+            pstatement.setString(10, infos.get(i).getWebsite());
+            pstatement.setString(11, infos.get(i).getTelefoon());
+            pstatement.setString(12, infos.get(i).getEmail());
+            pstatement.setDouble(13, ParseDouble(String.valueOf(infos.get(i).getPrijs())));
+            pstatement.setString(14, infos.get(i).getPersoon());
+                System.out.println(pstatement);
+            pstatement.executeUpdate();
+        }
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new NoQueryPossibleException("Insert " + TABLE_NAME + " can not be excecuted ");
+        }
+    }
 
+    public double ParseDouble(String strNumber) {
+   if (strNumber != null && strNumber.length() > 0) {
+       try {
+          return Double.parseDouble(strNumber);
+       } catch(Exception e) {
+          return 0;   // or some value to mark this field is wrong. or make a function validates field first ...
+       }
+   }
+   else return 0;
+}
+    
+    
     @Override
     public String getColumnString() {
        return "("+KEY+","+LOCATIE+","+STRAAT+","+NUMMER+","+POSTCODE+","+GEMEENTE+","+LAND+","+OMSCHRIJVING+", "+WIKIPEDIA_LINK+", "+WEBSITE+", "+TELEFOON+", "+E_MAIL+", "+PRIJS+", "+PERSOON+")";
     }
-
-    @Override
-    public String getValuesString(Informatie informatie) {
-        if(informatie == null){
-            throw new NullPointerException("The information you have given is null");
-        }
-        return "('"+informatie.getTitel()+"','"+ informatie.getLocatie()+"','"+informatie.getStraat()+"','"+informatie.getNummer()+"','"+informatie.getPostcode()+"','"+informatie.getGemeente()+"','"+informatie.getLand()+"','"+informatie.getOmschrijving()+"','"+informatie.getWiki_link()+"','"+informatie.getWebsite()+"','"+informatie.getTelefoon()+"','"+informatie.getEmail()+"',"+informatie.getPrijs()+",'"+informatie.getPersoon()+"')";    
-    }
-    
 }
